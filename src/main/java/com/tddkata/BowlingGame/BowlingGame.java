05f3961c998cca457fd3db4030969218b4de8e98
Alejandro Mateo <alejandro.mateo@clearpay.com>
2022-05-21 00:31:08 +0200

    BowlingGame: refactor

--- src/main/java/com/tddkata/BowlingGame/BowlingGame.java
+++ src/main/java/com/tddkata/BowlingGame/BowlingGame.java
@@ -6,28 +6,42 @@ import java.util.List;
 public class BowlingGame {
 
     public static final int PINS_IN_GAME = 10;
-    private int hitsCount;
-
-    private List<Integer> hits = new ArrayList();
+    public static final int MAX_STANDARD_ROLLS = 20;
+    private final List<Integer> hits = new ArrayList();
 
     public void roll(int pins) {
         if (pins > PINS_IN_GAME || pins < 0) {
             throw new RuntimeException();
         }
-        hitsCount += pins;
         hits.add(pins);
     }
 
     public int score() {
+        return calculateBaseScore() +
+               calculateSpareBonus();
+
+    }
+
+    private int calculateBaseScore() {
+        return hits.stream().mapToInt(Integer::intValue).sum();
+    }
+
+    private int calculateSpareBonus() {
         int spareBonus = 0;
-        for (int rollNumber = 0; rollNumber < 20; rollNumber++) {
-            if (rollNumber % 2 == 1
-                && (hits.get(rollNumber) + hits.get(rollNumber - 1)) == 10) {
-                spareBonus += hits.get(rollNumber+1);
+        for (int rollNumber = 0; rollNumber < MAX_STANDARD_ROLLS; rollNumber++) {
+            if (isRollNumberOdd(rollNumber)
+                && isSpare(rollNumber)) {
+                spareBonus += hits.get(rollNumber + 1);
             }
         }
+        return spareBonus;
+    }
 
-        return hitsCount + spareBonus;
+    private boolean isSpare(int rollNumber) {
+        return (hits.get(rollNumber) + hits.get(rollNumber - 1)) == 10;
+    }
 
+    private boolean isRollNumberOdd(int rollNumber) {
+        return rollNumber % 2 == 1;
     }
 }
