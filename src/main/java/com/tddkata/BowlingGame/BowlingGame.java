5bd560d3cd36c77bc9d6dd9b525fe7df56b42a28
Alejandro Mateo <alejandro.mateo@clearpay.com>
2022-05-21 00:03:53 +0200

    BowlingGame: green -> throws_exception_when_hits_max_of_pins

--- src/main/java/com/tddkata/BowlingGame/BowlingGame.java
+++ src/main/java/com/tddkata/BowlingGame/BowlingGame.java
@@ -5,6 +5,9 @@ public class BowlingGame {
     private int score;
 
     public void roll(int pins) {
+        if (pins > 10) {
+            throw new RuntimeException();
+        }
         score += pins;
     }
 
