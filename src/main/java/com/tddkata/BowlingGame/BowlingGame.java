da084d6df6e832d72340caa5e97d2ba923010c30
Alejandro Mateo <alejandro.mateo@clearpay.com>
2022-05-20 23:57:52 +0200

    BowlingGame: green -> return_one_when_hit_one_pin

---
+++ src/main/java/com/tddkata/BowlingGame/BowlingGame.java
@@ -0,0 +1,14 @@
+package com.tddkata.BowlingGame;
+
+public class BowlingGame {
+
+    private int score;
+
+    public void roll(int pins) {
+        score += pins;
+    }
+
+    public int score() {
+        return score;
+    }
+}
--- src/test/java/com/tddkata/BowlingGame/BowlingGameShould.java
+++ src/test/java/com/tddkata/BowlingGame/BowlingGameShould.java
@@ -4,33 +4,32 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
-class GameShould {
+class BowlingGameShould {
 
-    private Game game;
+    private BowlingGame bowlingGame;
 
     @BeforeEach
     void setUp() {
-        game = new Game();
+        bowlingGame = new BowlingGame();
     }
 
     @Test
     void return_zero_when_not_hit_any_pin() {
         for (int rollNumber = 0; rollNumber < 20; rollNumber++) {
-            game.roll(0);
+            bowlingGame.roll(0);
         }
 
-        assertThat(game.score()).isEqualTo(0);
+        assertThat(bowlingGame.score()).isEqualTo(0);
     }
 
     @Test
     void return_one_when_hit_one_pin() {
-        game.roll(1);
+        bowlingGame.roll(1);
         for (int rollNumber = 0; rollNumber < 19; rollNumber++) {
-            game.roll(0);
+            bowlingGame.roll(0);
         }
 
-        assertThat(game.score()).isEqualTo(-1);
+        assertThat(bowlingGame.score()).isEqualTo(1);
     }
 }
