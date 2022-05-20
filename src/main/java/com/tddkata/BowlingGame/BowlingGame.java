c18d62fd8ffb3bc3c45bedafa2f92aeeba0f65cd
Alejandro Mateo <alejandro.mateo@clearpay.com>
2022-05-21 00:04:33 +0200

    BowlingGame: refactor

--- src/main/java/com/tddkata/BowlingGame/BowlingGame.java
+++ src/main/java/com/tddkata/BowlingGame/BowlingGame.java
@@ -2,10 +2,11 @@ package com.tddkata.BowlingGame;
 
 public class BowlingGame {
 
+    public static final int PINS_IN_GAME = 10;
     private int score;
 
     public void roll(int pins) {
-        if (pins > 10) {
+        if (pins > PINS_IN_GAME) {
             throw new RuntimeException();
         }
         score += pins;
