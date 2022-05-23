f3863ee68fd3bdf1bb5fc0e4f66cc5d6f406998e
Alejandro Mateo <alejandro.mateo@clearpay.com>
2022-05-23 07:21:26 +0200

    BowlingGame: refactor to add Frame concept

--- src/main/java/com/tddkata/BowlingGame/BowlingGame.java
+++ src/main/java/com/tddkata/BowlingGame/BowlingGame.java
@@ -1,12 +1,31 @@
 package com.tddkata.BowlingGame;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class BowlingGame {
 
+    @RequiredArgsConstructor
+    @Getter
+    class Frame {
+        private final Integer roll1;
+        private final Integer roll2;
+
+        public boolean isSpare() {
+            return roll1 + roll2 == 10;
+        }
+
+        public int baseScore() {
+            return roll1 + roll2;
+        }
+    }
+
     public static final int PINS_IN_GAME = 10;
-    public static final int MAX_STANDARD_ROLLS = 20;
     private final List<Integer> rolls = new ArrayList<>();
 
     public void roll(int pins) {
@@ -17,27 +36,27 @@ public class BowlingGame {
     }
 
     public int score() {
-        return calculateBaseScore() +
-               calculateSpareBonus();
+        List<Frame> frames = buildFrames ();
 
-    }
+        int baseScore = frames.stream().mapToInt(Frame::baseScore).sum();
 
-    private int calculateBaseScore() {
-        return rolls.stream().mapToInt(Integer::intValue).sum();
-    }
-
-    private int calculateSpareBonus() {
-        int spareBonus = 0;
-        for (int rollNumber = 2; rollNumber < rolls.size(); rollNumber += 2) {
-            if (isSpare(rollNumber)) {
-                spareBonus += rolls.get(rollNumber);
+        int bonus = 0;
+        for (int frameNumber = 0; frameNumber < frames.size() -1; frameNumber++) {
+            if (frames.get(frameNumber).isSpare()) {
+                bonus += frames.get(frameNumber + 1).getRoll1();
             }
         }
-        return spareBonus;
+
+        return baseScore + bonus;
     }
 
-    private boolean isSpare(int rollNumber) {
-        return (rolls.get(rollNumber - 1) + rolls.get(rollNumber - 2)) == 10;
+    private List<Frame> buildFrames() {
+        List<Frame> frames = new ArrayList<>();
+        for (int rollNumber = 0; rollNumber < rolls.size(); rollNumber += 2) {
+            frames.add(new Frame(rolls.get(rollNumber), rolls.get(rollNumber + 1)));
+        }
+        return frames;
     }
+
 
 }
