6b108a160a61b84b163b97d645b9c6076de6b5a6
Alejandro Mateo <alejandro.mateo@clearpay.com>
2022-05-27 12:11:24 +0200

    romanNumerals: green case 4

--- src/main/java/com/tddkata/romanNumerals/RomanNumeralsCalculator.java
+++ src/main/java/com/tddkata/romanNumerals/RomanNumeralsCalculator.java
@@ -3,6 +3,9 @@ package com.tddkata.romanNumerals;
 public class RomanNumeralsCalculator {
 
     public String convertNumber(int number) {
+        if (number == 4) {
+            return "IV";
+        }
         if (number >= 1) {
             return "I" + convertNumber(number -1);
         }
