2e6e7f9eb6d4bdd2eebacc134a84ea424d343308
Alejandro Mateo <alejandro.mateo@clearpay.com>
2022-05-27 22:47:24 +0200

    GameOfLife: green - return_all_deaths_after_ome_step_when_only_one_cell_alive

--- src/main/java/com/tddkata/gameOfLife/GameOfLife.java
+++ src/main/java/com/tddkata/gameOfLife/GameOfLife.java
@@ -5,14 +5,50 @@ import lombok.Getter;
 public class GameOfLife {
 
     @Getter
-    private final boolean[][] board;
+    private boolean[][] board;
 
     public GameOfLife(boolean[][] board) {
         this.board = board;
     }
 
     public void nextGen() {
+        boolean[][] nextBoard = new boolean[board.length][board[0].length];
 
+        for (int x = 0; x < board.length; x++) {
+            for (int y = 0; y < board[x].length; y++) {
+                int nAlive = 0;
+                if (isCellAlive(board, x - 1, y - 1)) nAlive++;
+                if (isCellAlive(board, x - 1, y)) nAlive++;
+                if (isCellAlive(board, x - 1, y + 1)) nAlive++;
+                if (isCellAlive(board, x + 1, y - 1)) nAlive++;
+                if (isCellAlive(board, x + 1, y)) nAlive++;
+                if (isCellAlive(board, x + 1, y + 1)) nAlive++;
+                if (isCellAlive(board, x, y - 1)) nAlive++;
+                if (isCellAlive(board, x, y + 1)) nAlive++;
+
+                if (nAlive <= 2) {
+                    nextBoard[x][y] = false;
+                }
+
+            }
+        }
+
+        this.board = nextBoard;
+
+    }
+
+    private boolean isCellAlive(boolean[][] board, int x, int y) {
+        if (x < 0 || y < 0) {
+            return false;
+        }
+        if (x >= board.length) {
+            return false;
+        }
+        if (y >= board[x].length) {
+            return false;
+        }
+
+        return board[x][y];
     }
 
 }
