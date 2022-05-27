a1b94c426955df48d010be26b089832ad0ce4156
Alejandro Mateo <alejandro.mateo@clearpay.com>
2022-05-27 13:57:46 +0200

    christmasLights: red - turn off all lights after turn on all

--- src/main/java/com/tddkata/christmasLightsKata/ChristmasLights.java
+++ src/main/java/com/tddkata/christmasLightsKata/ChristmasLights.java
@@ -2,15 +2,17 @@ package com.tddkata.christmasLightsKata;
 
 public class ChristmasLights {
 
-    int[][] grid = new int[1000][1000];
+    public static final int ROWS = 1000;
+    public static final int COLUMNS = 1000;
+    int[][] grid = new int[ROWS][COLUMNS];
 
     public void turnOff(final Coordinate corner1, final Coordinate corner2) {
     }
 
     public int getNumberOfLightsOn() {
         int nLightsOn = 0;
-        for (int x = 0; x < 1000; x++) {
-            for (int y = 0; y < 1000; y++) {
+        for (int x = 0; x < ROWS; x++) {
+            for (int y = 0; y < COLUMNS; y++) {
                 nLightsOn += grid[x][y];
             }
         }
--- src/test/java/com/tddkata/christmasLightsKata/ChristmasLightsShould.java
+++ src/test/java/com/tddkata/christmasLightsKata/ChristmasLightsShould.java
@@ -28,4 +28,19 @@ class ChristmasLightsShould {
 
         assertThat(christmasLights.getNumberOfLightsOn()).isEqualTo(1000000);
     }
+
+    @Test
+    void return_2000_when_turn_on_first_two_rows() {
+        christmasLights.turnOn(Coordinate.of(0, 0), Coordinate.of(1, 999));
+
+        assertThat(christmasLights.getNumberOfLightsOn()).isEqualTo(2000);
+    }
+
+    @Test
+    void return_0_when_turn_off_all_lights_after_turn_on_all() {
+        christmasLights.turnOn(Coordinate.of(0, 0), Coordinate.of(999, 999));
+        christmasLights.turnOff(Coordinate.of(0, 0), Coordinate.of(999, 999));
+
+        assertThat(christmasLights.getNumberOfLightsOn()).isEqualTo(0);
+    }
 }
