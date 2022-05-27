d439081a77774d6b4d05aa8d1c962a156bd1b1d0
Alejandro Mateo <alejandro.mateo@clearpay.com>
2022-05-27 13:28:49 +0200

    christmasLights: init

---
+++ src/main/java/com/tddkata/christmasLightsKata/ChristmasLights.java
@@ -0,0 +1,4 @@
+package com.tddkata.christmasLightsKata;
+
+public class ChristmasLights {
+}
---
+++ src/test/java/com/tddkata/christmasLightsKata/ChristmasLightsShould.java
@@ -0,0 +1,12 @@
+package com.tddkata.christmasLightsKata;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChristmasLightsShould {
+
+
+}
