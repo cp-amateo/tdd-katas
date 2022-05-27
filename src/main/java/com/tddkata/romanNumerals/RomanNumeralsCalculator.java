876e36fcae261ed42b2821dff1932bbd68173265
Alejandro Mateo <alejandro.mateo@clearpay.com>
2022-05-27 10:40:04 +0200

    romanNumerals: init

---
+++ src/main/java/com/tddkata/romanNumerals/RomanNumeralsCalculator.java
@@ -0,0 +1,4 @@
+package com.tddkata.romanNumerals;
+
+public class RomanNumeralsCalculator {
+}
---
+++ src/test/java/com/tddkata/romanNumerals/RomanNumeralsCalculatorShould.java
@@ -0,0 +1,11 @@
+package com.tddkata.romanNumerals;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RomanNumeralsCalculatorShould {
+
+
+}
