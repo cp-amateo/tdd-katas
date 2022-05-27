dca094d0163af1fe59825356b1d984e957d5d70c
Alejandro Mateo <alejandro.mateo@clearpay.com>
2022-05-27 13:29:11 +0200

    christmasLights: red - all light turn off

--- src/test/java/com/tddkata/christmasLightsKata/ChristmasLightsShould.java
+++ src/test/java/com/tddkata/christmasLightsKata/ChristmasLightsShould.java
@@ -8,5 +8,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ChristmasLightsShould {
 
+    private ChristmasLights christmasLights;
 
+    @BeforeEach
+    void setUp() {
+        christmasLights = new ChristmasLights();
+    }
+
+    @Test
+    void turn_off_all_lights() {
+
+        christmasLights.turnOff(Coordinate.of(0, 0), Coordinate.of(0.0));
+
+        assertThat(christmasLights.getLightsLit()).isEqualTo(0);
+    }
 }
