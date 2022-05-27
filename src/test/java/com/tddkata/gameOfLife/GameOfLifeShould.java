cccc45cf346e05729fdd0eb1880fc11c93406b7d
Alejandro Mateo <alejandro.mateo@clearpay.com>
2022-05-27 22:33:16 +0200

    GameOfLife: red - return_all_deaths_after_ome_step_when_only_one_cell_alive

--- src/test/java/com/tddkata/gameOfLife/GameOfLifeShould.java
+++ src/test/java/com/tddkata/gameOfLife/GameOfLifeShould.java
@@ -8,7 +8,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GameOfLifeShould {
 
-
     @Test
     void return_all_deaths_when_board_is_all_death() {
         final boolean[][] board = new boolean[][]{
@@ -18,9 +17,27 @@ class GameOfLifeShould {
         };
 
         final GameOfLife gameOfLife = new GameOfLife(board);
-
         gameOfLife.nextGen();
 
         assertThat(gameOfLife.getBoard()).isEqualTo(board);
+    }
+
+    @Test
+    void return_all_deaths_after_ome_step_when_only_one_cell_alive() {
+        final boolean[][] board = new boolean[][]{
+                {false, false, false},
+                {false, true, false},
+                {false, false, false}
+        };
+
+        final GameOfLife gameOfLife = new GameOfLife(board);
+        gameOfLife.nextGen();
+
+        final boolean[][] boardExpected = new boolean[][]{
+                {false, false, false},
+                {false, false, false},
+                {false, false, false}
+        };
+        assertThat(gameOfLife.getBoard()).isEqualTo(boardExpected);
     }
 }
