8c4a781c55a9ce3c3a01fda8a1123643e8ad1620
Alejandro Mateo <alejandro.mateo@clearpay.com>
2022-05-27 12:20:57 +0200

    romanNumerals: refactor case 5

--- src/main/java/com/tddkata/romanNumerals/RomanNumeralsCalculator.java
+++ src/main/java/com/tddkata/romanNumerals/RomanNumeralsCalculator.java
@@ -1,18 +1,28 @@
 package com.tddkata.romanNumerals;
 
+import java.util.Map;
+
 public class RomanNumeralsCalculator {
+
+    private static final Map<Integer, String> symbols = Map.of(
+            0, "",
+            1, "I",
+            4, "IV",
+            5, "V"
+    );
+
 
     public String convertNumber(int number) {
         if (number >= 5) {
-            return "V" + convertNumber(number - 5);
+            return symbols.get(5) + convertNumber(number - 5);
         }
         if (number >= 4) {
-            return "IV" + convertNumber(number - 4);
+            return symbols.get(4) + convertNumber(number - 4);
         }
         if (number >= 1) {
-            return "I" + convertNumber(number - 1);
+            return symbols.get(1) + convertNumber(number - 1);
         }
 
-        return "";
+        return symbols.get(0);
     }
 }
