4bfd178037907b00e93d435fd643e6c3ddf42fba
Alejandro Mateo <alejandro.mateo@clearpay.com>
2022-05-27 12:06:23 +0200

    romanNumerals: refactor

--- src/main/java/com/tddkata/romanNumerals/RomanNumeralsCalculator.java
+++ src/main/java/com/tddkata/romanNumerals/RomanNumeralsCalculator.java
@@ -3,7 +3,9 @@ package com.tddkata.romanNumerals;
 public class RomanNumeralsCalculator {
 
     public String convertNumber(int number) {
-        if (number == 1) return "I";
+        if (number == 1) {
+            return "I" + convertNumber(0);
+        }
 
         return "";
     }
