afc33a0d3153e09760ce3ad9451fed1f79644d21
Alejandro Mateo <alejandro.mateo@clearpay.com>
2022-05-27 11:55:28 +0200

    romanNumerals: green - 1 case

--- src/main/java/com/tddkata/romanNumerals/RomanNumeralsCalculator.java
+++ src/main/java/com/tddkata/romanNumerals/RomanNumeralsCalculator.java
@@ -3,6 +3,9 @@ package com.tddkata.romanNumerals;
 public class RomanNumeralsCalculator {
 
     public String convertNumber(int number) {
+        if (number == 1) {
+            return "I";
+        }
         return "";
     }
 }
