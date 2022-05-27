d3e9d30798238d4508fa6590c5389844cc1ca1c2
Alejandro Mateo <alejandro.mateo@clearpay.com>
2022-05-27 22:28:51 +0200

    GameOfLife: green - return_all_deaths_when_board_is_all_death

--- src/test/java/com/tddkata/gameOfLife/GameOfLifeShould.java
+++ src/test/java/com/tddkata/gameOfLife/GameOfLifeShould.java
@@ -1,9 +1,26 @@
 package com.tddkata.gameOfLife;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameOfLifeShould {
 
 
+    @Test
+    void return_all_deaths_when_board_is_all_death() {
+        final boolean[][] board = new boolean[][]{
+                {false, false, false},
+                {false, false, false},
+                {false, false, false}
+        };
 
+        final GameOfLife gameOfLife = new GameOfLife(board);
+
+        gameOfLife.nextGen();
+
+        assertThat(gameOfLife.getBoard()).isEqualTo(board);
+    }
 }
