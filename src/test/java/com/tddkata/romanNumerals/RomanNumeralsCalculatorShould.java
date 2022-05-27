d238d30ab08f3ec7e3498b75bbd3f2d00a1941fa
Alejandro Mateo <alejandro.mateo@clearpay.com>
2022-05-27 11:47:19 +0200

    romanNumerals: red - '' case

--- src/test/java/com/tddkata/romanNumerals/RomanNumeralsCalculatorShould.java
+++ src/test/java/com/tddkata/romanNumerals/RomanNumeralsCalculatorShould.java
@@ -7,5 +7,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RomanNumeralsCalculatorShould {
 
+    private RomanNumeralsCalculator romanNumeralsCalculator = new RomanNumeralsCalculator();
 
+    @Test
+    void return_I_when_one() {
+        String romanNumber = romanNumeralsCalculator.convertNumber(0);
+        assertThat(romanNumber).isEqualTo("");
+    }
 }
