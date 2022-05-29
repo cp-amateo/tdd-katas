0f6098e74148b4d75feb6158ef8c30a71042b4ea
Alejandro Mateo <alejandro.mateo@clearpay.com>
2022-05-29 06:41:26 +0200

    Langston And: red - Langston And: red -throw_exception_when_grip_is_not_odd

--- src/main/java/com/tddkata/langstonAnt/AntPathGenerator.java
+++ src/main/java/com/tddkata/langstonAnt/AntPathGenerator.java
@@ -1,4 +1,5 @@
 package com.tddkata.langstonAnt;
 
 public class AntPathGenerator {
+
 }
--- src/test/java/com/tddkata/langstonAnt/AntPathGeneratorShould.java
+++ src/test/java/com/tddkata/langstonAnt/AntPathGeneratorShould.java
@@ -1,6 +1,15 @@
 package com.tddkata.langstonAnt;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 class AntPathGeneratorShould {
 
+    @Test
+    void throw_exception_when_grip_is_not_odd() {
+        assertThrows(IllegalArgumentException.class, () -> new AntPathGenerator(8).simulatePath("RL", 2));
+
+    }
 
 }
