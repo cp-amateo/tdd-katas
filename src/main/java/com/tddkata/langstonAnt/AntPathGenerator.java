13176c19f81cc8dff44900c61327455707ec97b2
Alejandro Mateo <alejandro.mateo@clearpay.com>
2022-05-29 06:41:52 +0200

    Langston And: green - Langston And: red -throw_exception_when_grip_is_not_odd

--- src/main/java/com/tddkata/langstonAnt/AntPathGenerator.java
+++ src/main/java/com/tddkata/langstonAnt/AntPathGenerator.java
@@ -1,5 +1,13 @@
 package com.tddkata.langstonAnt;
 
 public class AntPathGenerator {
+    public AntPathGenerator(int gripSize) {
+        if (gripSize % 2 == 0) {
+            throw new IllegalArgumentException();
+        }
 
+    }
+
+    public void simulatePath(final String rules, int steps) {
+    }
 }
